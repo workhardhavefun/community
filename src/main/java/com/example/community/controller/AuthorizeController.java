5705c34d3795f770c2fd1c96c5f3650388bb7c54
workhardhavefun <460066507@qq.com>
2019-07-29 09:45:13 +0800

    add check cookies

--- src/main/java/com/example/community/controller/AuthorizeController.java
+++ src/main/java/com/example/community/controller/AuthorizeController.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -33,7 +35,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -46,13 +48,14 @@ public class AuthorizeController {
         if (githubUser != null) {
             //登入成功，写cookies和session
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);//没手动写的话，Springboot会自动配置一个cookies
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";//重新定向index地址
         }else{
             //登入失败，重新登入
--- src/main/java/com/example/community/mapper/UserMapper.java
+++ src/main/java/com/example/community/mapper/UserMapper.java
@@ -4,6 +4,7 @@ package com.example.community.mapper;
 import com.example.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
@@ -11,4 +12,6 @@ public interface UserMapper {
 
     @Insert("insert into user (account_id,name,token,gmt_create,gmt_modified) values (#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified}) ")
     void insert(User user);
+    @Select("select * from user where token = #{token}")
+    User findBytoken(String token);
 }
