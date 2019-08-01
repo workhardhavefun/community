8432eaac1b753993ba420b8999bf511739265768
workhardhavefun <460066507@qq.com>
2019-08-01 19:19:04 +0800

    增加首页分页功能

--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.community.controller;
 
 
+import com.example.community.dto.PaginationDTO;
 import com.example.community.dto.QuestionDTO;
 import com.example.community.mapper.QuestionMapper;
 import com.example.community.mapper.UserMapper;
@@ -27,8 +28,10 @@ public class IndexController {
 
 
     @GetMapping("/")
-        public String index(HttpServletRequest request,
-                            Model model) {
+    public String index(HttpServletRequest request,
+                        Model model,
+                        @RequestParam(value = "page", defaultValue = "1") Integer page,
+                        @RequestParam(value = "size", defaultValue = "5") Integer size) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
             for (Cookie cookie : cookies) {
@@ -41,8 +44,8 @@ public class IndexController {
                     break;
                 }
             }
-        List<QuestionDTO> questionDTOList=questionService.list();
-        model.addAttribute("questions",questionDTOList);
+        PaginationDTO paginationDTO = questionService.list(page, size);
+        model.addAttribute("pagination", paginationDTO);
         return "index";
     }
 }
--- src/main/java/com/example/community/service/QuestionService.java
+++ src/main/java/com/example/community/service/QuestionService.java
@@ -1,11 +1,11 @@
 package com.example.community.service;
 
+import com.example.community.dto.PaginationDTO;
 import com.example.community.dto.QuestionDTO;
 import com.example.community.mapper.QuestionMapper;
 import com.example.community.mapper.UserMapper;
 import com.example.community.model.Question;
 import com.example.community.model.User;
-import com.sun.javafx.scene.control.skin.VirtualFlow;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,16 +20,31 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions=questionMapper.list();
-        List<QuestionDTO> questionDTOList=new ArrayList<>();
+    public PaginationDTO list(Integer page, Integer size) {
+
+
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount=questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+        if(page<1){
+            page=1;
+        }
+        if(page>paginationDTO.getTotalPage()){
+            page=paginationDTO.getTotalPage();
+        }
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset,size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+
         for (Question question : questions) {
-            User user=userMapper.findById(question.getCreator());
+            User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
-            BeanUtils.copyProperties(question,questionDTO);
+            BeanUtils.copyProperties(question, questionDTO);
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
     }
 }
